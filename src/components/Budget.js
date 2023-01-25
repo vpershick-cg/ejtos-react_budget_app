920dbacb4e2be54b7f9da4334a4ab018da0b6354
My Name <me@user.test>
2023-01-25 13:17:53 -0500

    IBM React Lab steps 4 and 5, add currency prefix to allocation input and budget value input

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, currency } = useContext(AppContext);
     const { dispatch } = useContext(AppContext);
 
     const handleSetBudget = (budget) => {
@@ -14,17 +14,16 @@ const Budget = () => {
 
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-
-            <input
-                required='required'
-                type='number'
-                id='budget'
-                value={budget}
-                step={10}
-                style={{ marginLeft: '2rem' , size: 10}}
-                onChange={(event) => handleSetBudget(event.target.value)}>
-            </input>
+            <span>Budget: {currency}
+                <input
+                    required='required'
+                    type='number'
+                    id='budget'
+                    value={budget}
+                    step={10}
+                    onChange={(event) => handleSetBudget(event.target.value)}>
+                </input>
+            </span>
         </div>
     );
 };
